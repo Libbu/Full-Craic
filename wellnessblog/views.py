c775b41f691b5287f44c56adb0edbf37addb4eed
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-06 14:51:47 +0000

    migrated session and user models

--- wellnessblog/views.py
+++ wellnessblog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import UserProfile, Session
-from .forms import UserProfileForm, SessionForm
+#from .forms import UserProfileForm, SessionForm
 
 # Create your views here.
 
