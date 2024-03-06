05ecf94014925884212556c9fe1c890e83422189
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-06 14:38:05 +0000

    preparing to merge with updated models and signup page

--- wellnessblog/models.py
+++ wellnessblog/models.py
@@ -3,6 +3,3 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
-class Blog(models.Model):
-    title = models.CharField(max_length = 100)
-    image = CloudinaryField('image', default = 'placeholder')
