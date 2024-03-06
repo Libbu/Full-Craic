a9adf5a1d27cef9d70ee63ff00a5104b33dcc53c
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-03-06 13:52:48 +0000

    had a CRAIC at - added signup form html and css

--- wellnessblog/models.py
+++ wellnessblog/models.py
@@ -2,7 +2,3 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
-
-class Blog(models.Model):
-    title = models.CharField(max_length = 100)
-    image = CloudinaryField('image', default = 'placeholder')
