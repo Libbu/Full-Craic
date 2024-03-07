ba84d6b114b8dc9b298605ba46d7d05bc63b9c54
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-03-07 21:44:07 +0000

    preparing to merge to main

--- thrivingtogether/settings.py
+++ thrivingtogether/settings.py
@@ -104,15 +104,15 @@ WSGI_APPLICATION = 'thrivingtogether.wsgi.application'
 # Database will use local sqlite in dev and elephantsql in prod
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# if "debug in os.environ":
-#     DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# else:
-DATABASES = {
+if "debug in os.environ":
+     DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+else:
+    DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DB_URL'))
 }
 
