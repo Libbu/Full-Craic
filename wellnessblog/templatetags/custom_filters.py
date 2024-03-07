b1229a56bda98f317a61f20eb5f2b22e271d1ae2
James Price <james.a.t.price@gmail.com>
2024-03-07 16:31:14 +0000

    Fix: Stars displaying

--- wellnessblog/templatetags/custom_filters.py
+++ wellnessblog/templatetags/custom_filters.py
@@ -4,8 +4,13 @@ register = template.Library()
 
 @register.filter
 def get_range(value):
+    if value is None:
+        return range(0)
     return range(value)
+
 
 @register.filter
 def get_complement_range(value):
+    if value is None:
+        return range(5)
     return range(5 - value)
